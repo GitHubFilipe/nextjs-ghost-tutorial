67dd5943bcab07c52c2a1e4408606ff3af7ba80e
GitHubFilipe <meetfm@gmail.com>
2020-11-29 22:19:41 +0100

    Added global and component level styles

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import styles from '../styles/Home.module.css'
+
 function Home() {
-  return <div>Welcome at ghost next.js tutorial</div>
+  return (
+    <>
+      <h1>My Blog</h1>
+      <h2 className={styles.caption}>Checking module style here</h2>
+      <div>Welcome at ghost next.js tutorial</div>
+    </>
+  )
 }
 
 export default Home
